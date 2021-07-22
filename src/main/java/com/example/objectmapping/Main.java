0e676cbcffd3925104bd51132d025804b81cba54
Ivan Yonkov <ivanynkv@gmail.com>
2021-07-22 20:50:23 +0300

    examples

--- src/main/java/com/example/objectmapping/Main.java
+++ src/main/java/com/example/objectmapping/Main.java
@@ -1,17 +1,22 @@
 package com.example.objectmapping;
 
-import com.example.objectmapping.models.dto.EmployeeCreateRequest;
-import com.example.objectmapping.models.dto.EmployeeCreateResponse;
-import com.example.objectmapping.models.dto.ManagerCollection;
-import com.example.objectmapping.models.dto.ManagerDto;
+import com.example.objectmapping.models.dto.*;
+import com.example.objectmapping.models.dto.datetimetest.DateTimeSource;
+import com.example.objectmapping.models.dto.datetimetest.DateTimeTarget;
+import com.example.objectmapping.models.dto.firstlastnametest.FirstLastNameSource;
+import com.example.objectmapping.models.dto.firstlastnametest.FullNameTarget;
+import com.example.objectmapping.repositories.EmployeeRepository;
 import com.example.objectmapping.services.EmployeeService;
 import com.example.objectmapping.services.util.FormatConverter;
 import com.example.objectmapping.services.util.FormatConverterFactory;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class Main implements CommandLineRunner {
@@ -20,14 +25,47 @@ public class Main implements CommandLineRunner {
 
     private final FormatConverterFactory factory;
 
-    public Main(EmployeeService employeeService, FormatConverterFactory factory) {
+    private final EmployeeRepository employeeRepository;
+
+    private final ModelMapper modelMapper;
+
+    public Main(EmployeeService employeeService, FormatConverterFactory factory, EmployeeRepository employeeRepository, ModelMapper modelMapper) {
         this.employeeService = employeeService;
         this.factory = factory;
+        this.employeeRepository = employeeRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        Stream.of(1,2,3,4,6)
+                .map(el -> el * 2)
+                .peek(el -> System.out.println(el * 2))
+                .collect(Collectors.toList());
+
+
+
+//        var source = new DateTimeSource();
+//        source.setDate("2020-11-11 12:34:56");
+//
+//        DateTimeTarget target = this.modelMapper.map(
+//                source,
+//                DateTimeTarget.class
+//        );
+
+
+        var source = new FirstLastNameSource();
+        source.setFirstName("pesho");
+        source.setLastName("petrov");
+
+        var target = this.modelMapper.map(
+                source,
+                FullNameTarget.class
+        );
+
+        List<EmployeeAverageSalaryDto> allWithAvgSalary = this.employeeRepository.findAllWithAvgSalary();
+//        "2020-05-05 16:40:50"
         var sc = new Scanner(System.in);
 
         System.out.println("Enter format type: ");
@@ -36,7 +74,14 @@ public class Main implements CommandLineRunner {
         FormatConverter converter = this.factory.create(formatType);
         converter.setPrettyPrint();
 
+        TestDto testDto = converter.deserializeFromFile(
+                "./src/main/resources/test-za-dati.xml",
+                TestDto.class
+        );
+
         var line = sc.nextLine();
+
+
 
         while (!line.equals("end")) {
             var cmdParts = line.split(" ", 2);
--- src/main/java/com/example/objectmapping/services/EmployeeServiceImpl.java
+++ src/main/java/com/example/objectmapping/services/EmployeeServiceImpl.java
@@ -8,7 +8,10 @@ import com.example.objectmapping.repositories.EmployeeRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 
@@ -55,6 +58,23 @@ public class EmployeeServiceImpl implements EmployeeService {
                 ),
                 EmployeeCreateResponse.class
         );
+    }
+
+    @Transactional
+    public void giveAway(long managerId) {
+        var manager = this.employeeRepository.findById(managerId).orElseThrow();
+
+        var halfSalary = manager.getSalary().divide(new BigDecimal(2), RoundingMode.CEILING);
+        var forEachEmployee = halfSalary.divide(BigDecimal.valueOf(manager.getSubordinates().size()), RoundingMode.CEILING);
+
+        manager.getSubordinates().forEach(employee -> {
+            employee.setSalary(employee.getSalary().add(forEachEmployee));
+            this.employeeRepository.save(employee);
+        });
+
+        manager.setSalary(manager.getSalary().subtract(halfSalary));
+
+        this.employeeRepository.save(manager);
     }
 
 
